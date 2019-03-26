6a9af22a35fe80ce9e4a3ebac403bf1035e44019
wynn <wynn@MacBook-Pro.local>
2019-03-26 18:46:56 +0800

    添加资源文件，创建初始场景

---
+++ Assets/Scripts/HGYXFadeIn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HGYXFadeIn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Finish() {
+		// Debug.Log("完成");
+	}
+}
